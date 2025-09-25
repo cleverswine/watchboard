5cf0ab645f1c0744e670d3f454ff038b5632dd69
Kevin Noone <kevin.noone@surescripts.com>
2025-09-25 16:06:25 -0500

    vuetify

---
+++ watchboard/client-app-vuetify/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app :theme="theme">
+    <v-app-bar>
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" />
+      <v-toolbar-title>My files</v-toolbar-title>
+      <v-spacer />
+      <v-btn
+        :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+        slim
+        text="Toggle Theme"
+        @click="onClick"
+      />
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer">
+      <v-list>
+        <v-list-item title="Navigation drawer" />
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+
+  const theme = ref('dark')
+  const drawer = ref(true)
+
+  function onClick () {
+    theme.value = theme.value === 'light' ? 'dark' : 'light'
+  }
+</script>
---
+++ watchboard/client-app-vuetify/src/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <ItemGrid />
+  <HelloWorld />
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
