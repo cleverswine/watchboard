df5ccc158fa3c0768f1a3bacaf197fff55b9a129
cleverswine <cleverswine@gmail.com>
2025-09-26 15:31:22 -0400

    more vuertify

--- watchboard/client-app-vue/src/App.vue
+++ watchboard/client-app-vue/src/App.vue
@@ -1,17 +1,62 @@
 <template>
   <v-app :theme="theme">
-    <v-app-bar>
-      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" />
-      <v-toolbar-title>My files</v-toolbar-title>
-      <v-spacer />
-      <v-text-field placeholder="search..."></v-text-field>
-      <v-spacer />
-      <v-btn :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" slim text="Toggle Theme" @click="onClick" />
+    <v-app-bar density="comfortable">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" />
+      </template>
+
+      <v-toolbar-title>My Stuff</v-toolbar-title>
+
+      <div class="justify-center w-25">
+        <v-text-field density="compact" placeholder="Search" prepend-inner-icon="mdi-magnify" variant="solo" flat hide-details single-line></v-text-field>
+      </div>
+      <v-spacer></v-spacer>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn icon="mdi-account-circle" v-bind="props"></v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="i in 6" :key="i" :value="i">
+            <v-list-item-title>Stuff for {{ i }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-btn :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" @click="onClick" />
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer">
+      <v-container>
+        <div>Filter</div>
+
+        <v-divider></v-divider>
+
+        <v-select density="default" v-model="value" :items="items" label="country" multiple></v-select>
+        <v-select density="comfortable" v-model="value" :items="items" label="language" multiple></v-select>
+        <v-select density="compact" v-model="value" :items="items" label="service" multiple></v-select>
+
+        <div class="v-row">
+          <div class="v-col-6">
+            <v-btn block variant="elevated" color="blue-darken-4">Apply</v-btn>
+          </div>
+          <div class="v-col-6">
+            <v-btn block variant="tonal">Clear</v-btn>
+          </div>
+        </div>
+      </v-container>
+
       <v-list>
-        <v-list-item title="Navigation drawer" />
+        <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+        <v-divider></v-divider>
+        <v-list-item>
+          <v-select density="default" v-model="value" :items="items" label="country" multiple></v-select>
+        </v-list-item>
+        <v-list-item>
+          <v-select density="comfortable" v-model="value" :items="items" label="language" multiple></v-select>
+        </v-list-item>
+        <v-list-item>
+          <v-select density="compact" v-model="value" :items="items" label="service" multiple></v-select>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
@@ -25,6 +70,10 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
+import { shallowRef } from 'vue'
+
+const items = shallowRef(['foo', 'bar', 'fizz', 'buzz'])
+const value = shallowRef(['foo', 'bar', 'fizz', 'buzz'])
 
 const theme = ref('dark')
 const drawer = ref(true)
--- watchboard/client-app-vue/src/views/HomeView.vue
+++ watchboard/client-app-vue/src/views/HomeView.vue
@@ -1,22 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { shallowRef } from 'vue'
+
+const items = shallowRef(['foo', 'bar', 'fizz', 'buzz'])
+const value = shallowRef(['foo', 'bar', 'fizz', 'buzz'])
+</script>
 
 <template>
   <v-container>
-    <v-toolbar density="compact">
-      <v-btn icon="mdi-filter">
-        <v-menu activator="parent">
+    <v-toolbar elevation="8" density="compact">
+      <v-toolbar-items>
+        <v-btn color="medium-emphasis" density="comfortable" icon="mdi-crosshairs-gps"></v-btn>
+
+        <v-btn class="ms-1" color="medium-emphasis" density="comfortable" icon="mdi-dots-vertical"></v-btn>
+
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn icon="mdi-filter-menu" variant="text" v-bind="props"></v-btn>
+          </template>
+
           <v-list>
-            <v-list-item v-for="n in 14" :key="n" :value="n">
-              <v-list-item-title>{{ n }}</v-list-item-title>
+            <v-list-item v-for="i in 6" :key="i" :value="i">
+              <v-list-item-title>{{ i }}</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
-      </v-btn>
-      <v-text-field density="compact" placeholder="Search" prepend-inner-icon="mdi-magnify"></v-text-field>
+
+        <v-text-field
+          class="me-2"
+          density="compact"
+          placeholder="Search"
+          prepend-inner-icon="mdi-magnify"
+        ></v-text-field>
+      </v-toolbar-items>
     </v-toolbar>
 
     <v-row>
-      <v-col v-for="n in 14" :key="n" lg="2" md="4" sm="6" xl="1" xs="12">
+      <v-col v-for="n in 14" :key="n" lg="2" md="4" sm="6" xl="2  " xs="12">
         <v-card link to="/ItemDetail">
           <v-img class="align-end text-white" cover src="@/assets/logo.png">
             <v-card-title>Top 10 Australian beaches Top 10 Australian beaches</v-card-title>
