c9b466f5363b4c72113f0ce5d2acc430a619ef5b
cleverswine <cleverswine@gmail.com>
2025-10-01 17:34:59 -0400

    filters

--- watchboard/client-app-vue/src/views/HomeView.vue
+++ watchboard/client-app-vue/src/views/HomeView.vue
@@ -3,51 +3,48 @@ import { shallowRef } from 'vue'
 
 const items = shallowRef(['foo', 'bar', 'fizz', 'buzz'])
 const value = shallowRef(['foo', 'bar', 'fizz', 'buzz'])
-const selected = shallowRef('foo')
+const chip = shallowRef(true)
 </script>
 
 <template>
   <v-container>
-    <v-toolbar density="compact">
-      <v-btn>
-        <v-icon>mdi-format-align-left</v-icon>
-      </v-btn>
-      <v-btn>
-        <v-icon>mdi-format-align-center</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-row>
-      <v-col cols="2">
-        <v-btn-toggle density="compact">
-          <v-btn>
-            <v-icon>mdi-format-align-left</v-icon>
-          </v-btn>
-          <v-btn>
-            <v-icon>mdi-format-align-center</v-icon>
-          </v-btn>
-        </v-btn-toggle>
-      </v-col>
-    </v-row>
-    <v-row align="center">
-      <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
-        <v-select multiple clearable density="compact" hide-details label="Type" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" variant="solo-filled"></v-select>
-      </v-col>
-      <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
-        <v-select multiple clearable density="compact" hide-details label="Country" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" variant="solo-filled"></v-select>
-      </v-col>
-      <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
-        <v-select multiple clearable density="compact" hide-details label="Language" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" variant="solo-filled"></v-select>
-      </v-col>
-      <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
-        <v-select multiple clearable density="compact" hide-details label="Source" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" variant="solo-filled"></v-select>
-      </v-col>
-      <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
-        <v-select multiple clearable density="compact" hide-details label="Status" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" variant="solo-filled"></v-select>
-      </v-col>
-      <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
-        <v-btn variant="tonal">Clear All</v-btn>
-      </v-col>
-    </v-row>
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-title>
+          <span class="text-center">
+            <v-chip v-if="chip" class="ma-2" size="small" closable @click:close="chip = false"> Closable </v-chip>
+            <v-btn density="compact" v-if="!chip" color="primary" @click="chip = true"> Reset Chip </v-btn>
+          </span>
+          <span class="text-center">
+            <v-chip v-if="chip" class="ma-2" size="small" closable @click:close="chip = false"> Closable </v-chip>
+            <v-btn density="compact" v-if="!chip" color="primary" @click="chip = true"> Reset Chip </v-btn>
+          </span>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <v-row align="center">
+            <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
+              <v-select multiple clearable density="compact" hide-details label="Type" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" variant="solo-filled"></v-select>
+            </v-col>
+            <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
+              <v-select multiple clearable density="compact" hide-details label="Country" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" variant="solo-filled"></v-select>
+            </v-col>
+            <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
+              <v-select multiple clearable density="compact" hide-details label="Language" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" variant="solo-filled"></v-select>
+            </v-col>
+            <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
+              <v-select multiple clearable density="compact" hide-details label="Source" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" variant="solo-filled"></v-select>
+            </v-col>
+            <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
+              <v-select multiple clearable density="compact" hide-details label="Status" :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" variant="solo-filled"></v-select>
+            </v-col>
+            <v-col cols="2" xs="12" sm="6" md="4" lg="2" xl="2">
+              <v-btn variant="tonal">Clear All</v-btn>
+            </v-col>
+          </v-row>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
     <v-row>
       <v-col v-for="n in 14" :key="n" lg="2" md="4" sm="6" xl="2  " xs="12">
         <v-card link to="/ItemDetail">
